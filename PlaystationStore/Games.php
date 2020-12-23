d7073699b76bb8e73cce5e76d8d4674288334a4a
0xWaleed <20063083+0xWaleed@users.noreply.github.com>
2020-12-23 05:45:01 +0300

    formatting and cleaning

--- PlaystationStore/Games.php
+++ PlaystationStore/Games.php
@@ -1,26 +1,32 @@
 <?php
+
 class Games extends PlaystationStore
 {
     public function __construct($region = 'US')
     {
         parent::__construct($region);
     }
+
     public function freeToPlay($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-FREETOPLAYSEEALL', $start, $size, 'games');
     }
+
     public function onlyOnPlaystation($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-ONLYONPSTIONPS4', $start, $size, 'games');
     }
+
     public function priceDrop($start, $size)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-PRICEDROPSCHI', $start, $size, 'games,bundles,addons');
     }
+
     public function newRelease($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-COMINGSOON', $start, $size, 'bundles');
     }
+
     public function digitalZone($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-DIGITALZONE', $start, $size, null);
--- PlaystationStore/PSPlus.php
+++ PlaystationStore/PSPlus.php
@@ -1,28 +1,29 @@
 <?php
+
 class PSPlus extends PlaystationStore
 {
-    public function __construct($region = 'US')
-    {
-        parent::__construct($region);
-    }
     public function monthlyGames($start = 0, $size = 30)
     {
         $data = $this->GetDataFromStoreAsObject('STORE-MSF75508-PLUSINSTANTGAME', $start, $size, null);
         $dataAsArray = (array)$data;
 
-        $filtered = array_filter($dataAsArray, function ($key){
+        $filtered = array_filter($dataAsArray, function ($key)
+        {
             return strpos($key->name, 'PlayStation') === false;
         });
         return $filtered;
     }
+
     public function themes($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-PLUSTHEMESAVATAR', $start, $size, 'themes');
     }
+
     public function avatars($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-PLUSTHEMESAVATAR', $start, $size, 'avatars');
     }
+
     public function indieGames($start = 0, $size = 30)
     {
         return $this->GetDataFromStoreAsObject('STORE-MSF75508-INDIEGAMES', $start, $size, null);
